ae4bea43ac5270940ea91923758d57cdadb896e5
unknown <Usuario@HPPAVILION.auttic.local>
2022-04-06 18:39:07 +0200

    Todo ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import Stock from "./components/Stock";
 import AlertDialog from "./components/AlertDialog";
+import SnackBar from "./components/SnackBar"
+
+
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-		<AlertDialog/>
-        <Stock texto="Añadir" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				{/* Dialog y Confirmar */}
+				<div className="alertDialog">
+					<AlertDialog mensajeDialog=" Esto es un componente de Material UI en React."/>
+					<SnackBar/>
+				</div>
+				{/* Mi componente */}
+				<Stock texto="Añadir" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/AlertDialog.js
+++ src/components/AlertDialog.js
@@ -6,44 +6,45 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
-export default function AlertDialog() {
-  const [open, setOpen] = React.useState(false);
+export default function AlertDialog(props) {
+    const {mensajeDialog} = props
+    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div>
-      <Button variant="contained" onClick={handleClickOpen}>
-        Mensaje
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"¿Estás de acuerdo?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Esto es un componente de Material UI en React.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Denegar</Button>
-          <Button onClick={handleClose} autoFocus>
-            Aceptar
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+    return (
+        <div>
+        <Button variant="contained" onClick={handleClickOpen}>
+            Mensaje
+        </Button>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+            {"¿Estás de acuerdo?"}
+            </DialogTitle>
+            <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+                {mensajeDialog}
+            </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose}>Denegar</Button>
+            <Button onClick={handleClose} autoFocus>
+                Aceptar
+            </Button>
+            </DialogActions>
+        </Dialog>
+        </div>
+    );
 }
 
